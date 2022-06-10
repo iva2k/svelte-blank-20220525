175de90a4131baa7a971249275fcb47dedd5388c
iva2k <iva2k@yahoo.com>
2022-06-09 17:56:36 -0700

    Fix Stylelint

--- src/routes/todos/index.svelte
+++ src/routes/todos/index.svelte
@@ -190,6 +190,14 @@
     }
   }
 
+  @media screen and (prefers-reduced-motion: reduce) {
+    .delete:hover,
+    .delete:focus {
+      transition: none;
+      opacity: 1;
+    }
+  }
+
   .delete:hover,
   .delete:focus {
     transition: opacity 0.2s;
@@ -201,6 +209,14 @@
     right: 0;
     opacity: 0;
     background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 2H3.5C2.67158 2 2 2.67157 2 3.5V20.5C2 21.3284 2.67158 22 3.5 22H20.5C21.3284 22 22 21.3284 22 20.5V3.5C22 2.67157 21.3284 2 20.5 2Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M17 2V11H7.5V2H17Z' fill='white' stroke='white' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M13.5 5.5V7.5' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3Cpath d='M5.99844 2H18.4992' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3C/svg%3E%0A");
+  }
+
+  @media screen and (prefers-reduced-motion: reduce) {
+    .todo input:focus + .save,
+    .save:focus {
+      transition: none;
+      opacity: 1;
+    }
   }
 
   @media screen and (prefers-reduced-motion: reduce) {
