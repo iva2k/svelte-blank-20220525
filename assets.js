c509a5f5a813cc44ae85cabf4bf941ddd84f39f8
Ilya I <iva2k@yahoo.com>
2022-06-20 21:19:11 -0700

    move assets from svelte.config.js & package.json to assets.mjs

--- assets.js
+++ assets.js
@@ -7,6 +7,10 @@ const assets = [
   // List of assets to be copied (can use glob / * wildcards)
   // Useful e.g. for dynamically loaded stylesheets.
   // Example: { src: 'node_modules/<module>/dist/*.css', dest: 'static/vendor/<module>/themes' }
+  {
+    src: 'node_modules/@shoelace-style/shoelace/dist/themes/*.css',
+    dest: 'static/vendor/shoelace/themes'
+  }
 ];
 
 export default assets;
